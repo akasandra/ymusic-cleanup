034bdcb60c80904b04a8e30fe7af40c9acce3343
Alexandra <5148375+akasandra@users.noreply.github.com>
2025-07-11 17:15:55 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import logging
+from yandex_music import Client
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+#client = Client('token').init()
+
+print('OK')
